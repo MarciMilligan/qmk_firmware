813c00cb3069a32136f8ee42f776e7388e7e1cb1
Marceline Milligan <marcelinemilligan@gmail.com>
2024-07-07 12:45:56 -0600

    Use new layout on Preonic

--- keyboards/preonic/keymaps/MarciMilligan/keymap.c
+++ keyboards/preonic/keymaps/MarciMilligan/keymap.c
@@ -20,7 +20,6 @@
 enum preonic_layers
 {
   _DEFAULT,
-  _MAC,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -29,7 +28,6 @@ enum preonic_layers
 enum preonic_keycodes
 {
   DEFAULT = SAFE_RANGE,
-  MAC,
   LOWER,
   RAISE
 };
@@ -39,24 +37,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
   [_DEFAULT] = LAYOUT_preonic_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-  ),
-
-  [_MAC] = LAYOUT_preonic_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LGUI, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   [_LOWER] = LAYOUT_preonic_grid(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F12,
     _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-    CW_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    CW_TOGG, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_VOLD, KC_VOLU, KC_BRIU,
     _______, _______, _______, KC_RGUI, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
 
@@ -64,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F12,
     _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    CW_TOGG, _______, _______, _______, _______, _______, _______, KC_BSLS, _______, _______, _______, _______,
+    CW_TOGG, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_BRID, KC_VOLD, KC_VOLU, KC_BRIU,
     _______, _______, _______, KC_RGUI, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
 
@@ -73,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     _______, QK_BOOT, DB_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, DEFAULT, _______, MAC,     _______, _______, _______, _______, _______, _______, _______, _______
+    _______, DEFAULT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
 
@@ -87,14 +77,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         set_single_persistent_default_layer(_DEFAULT);
       }
       return false;
-      break;
-    case MAC:
-      if (record->event.pressed)
-      {
-        set_single_persistent_default_layer(_MAC);
-      }
-      return false;
-      break;
     case LOWER:
       if (record->event.pressed)
       {
@@ -107,7 +89,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     case RAISE:
       if (record->event.pressed)
       {
@@ -120,7 +101,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     }
   return true;
 };
